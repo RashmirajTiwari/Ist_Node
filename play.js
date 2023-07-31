09ff7a5a589079ac295cee4a0b534d451d4e8709
Rashmiraj Tiwari <rashmirajt@athmin.com>
2023-07-31 22:44:21 +0530

    Worked on Destructuring

--- play.js
+++ play.js
@@ -23,3 +23,14 @@ console.log(fruits.map((fruit)=>{
 const hobbies=['Cooking','sports'];
 hobbies.push('programming');
 console.log(hobbies);
+
+//Destructuring
+
+const [hobby1,hobby2]=hobbies;
+console.log(hobby1,hobby2)
+
+const {Name,Age}=student;
+console.log(Name,Age)
+
+const result=({Name})=>Name;
+console.log(result(student))
